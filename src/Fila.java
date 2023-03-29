499c088666933f7f3435703a420414d9aa862d42
ads <ads@fatec.com>
2023-03-29 12:05:45 -0300

    Initial commit

---
+++ src/Fila.java
@@ -0,0 +1,13 @@
+public class Fila {
+    private String print;
+
+    private print(){
+
+    }
+    public static print getInstance(){
+        id(instance == null){
+            instance = new print();
+        }
+        return instance;
+    }
+}
